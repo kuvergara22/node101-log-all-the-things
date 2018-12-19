b87716cccdbbaa045329d82e3a8596a9b31446cb
User <user@Users-MacBook-Pro-3.local>
2018-12-19 15:28:43 -0800

    adds initial project files

---
+++ server/README.md
@@ -0,0 +1,9 @@
+This project will builds a custom logger that will log some information about each request it receives. 
+Also expose an endpoint on the server so it would be easy for anyone to retrieve the latest log data from the web.
+make sure to include this headers 
+Agent
+Time
+Method
+Resource
+Version
+Status
--- server/app.js
+++ server/app.js
@@ -2,18 +2,65 @@ const express = require('express');
 const fs = require('fs');
 const app = express();
 
-app.use((req, res, next) => {
-// write your logging code here
 
+fs.writeFile('log.csv', "Agent,Time,Method,Resource,Version,Status", (err) => {
+    if (err) throw err;
+    // console.log('something went wrong');
+  });
+
+  let logallData = [];
+
+  app.use((req, res, next) => {
+    // write your logging code here
+    var time = new Date();
+    var loggin = [];
+    var fileObj = {};
+
+    loggin.push(req.headers['user-agent']);
+    
+    fileObj.Agent = req.headers['user-agent'];
+    
+    loggin.push(time.toISOString());
+    
+    fileObj.Time = time.toISOString();
+    
+    loggin.push(req.method);
+    
+    fileObj.Method = req.method;
+    
+    loggin.push(req.originalUrl);
+    
+    fileObj.Resource = req.originalUrl;
+    
+    loggin.push("HTTP/" + req.httpVersion);
+    
+    fileObj.Version = "HTTP/" + req.httpVersion;
+    
+    loggin.push(res.statusCode);
+    
+    fileObj.Status = res.statusCode;
+
+    logallData.push(fileObj);
+     
+    fs.appendFile(__dirname + '/log.csv', loggin, (err) => {
+        if (err) throw err;
+        
+      });
+    
+      console.log(loggin.toString());
+
+    next();
 });
 
 app.get('/', (req, res) => {
 // write your code to respond "ok" here
-
+res.send("Ok. Request received.")
 });
 
 app.get('/logs', (req, res) => {
 // write your code to return a json object containing the log data here
+
+res.send(logallData);
 
 });
 
